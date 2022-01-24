8cb9b2395de6050e825812bb24bae32f54f6b0a7
trystankk <45819499+trystankk@users.noreply.github.com>
2022-01-24 23:03:13 +0800

    Update Account.java

--- BankAccountQ2/Account.java
+++ BankAccountQ2/Account.java
@@ -1,3 +1,11 @@
+/* Account class is the main class, AccountSystem is sub-class which store the menu list and functions. 
+   Import scanner as we need user to input. Public static void main at the main class. For the main class 
+   will prompt for user’s name and account balance. Double for UserBalance because it has decimal point. 
+   Then, ‘while’ loop will keep looping if user input negative amount, until user input the correct amount. 
+   To bring both classes link together, PersonalInfo as an object to store AccountSystem class. AccountSystem 
+   class is not a main. The  menu list using ‘switch’ in ‘do’ loop to keep repeating the menu until user 
+   input ‘99’ to stop the program.*/
+
 package BankAccount;
 
 import java.util.Scanner;
